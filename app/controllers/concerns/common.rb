f4e8ba0c4a6e6127db9c99596ebd2c33f9de241e
gitk47320 <34836412+gitk47320@users.noreply.github.com>
2019-10-08 21:59:09 +0900

    'github'

---
+++ app/controllers/concerns/common.rb
@@ -0,0 +1,53 @@
+module Common
+  require 'net/http'
+  require 'uri'
+  require 'json'
+
+  def getlatlon(place)
+    reqlatlonprm  = URI.encode_www_form(
+      {
+        appid: $clientid, 
+        query: place, 
+        output: 'json'
+      }
+    )
+    reqlatlonuri  = URI.parse("https://map.yahooapis.jp/search/local/V1/localSearch?#{reqlatlonprm}")
+    reslatlon     = Net::HTTP.get(reqlatlonuri)
+    reslatlon_j   = JSON.parse(reslatlon)
+    latloninfo    = reslatlon_j["Feature"][0]["Geometry"]["Coordinates"].split(",")
+    return latloninfo
+  end
+
+  def getAddress(lat, lon)
+    reqAddressprm  = URI.encode_www_form(
+      {
+        appid: $clientid,
+        lat: lat,
+        lon: lon,
+        output: 'json'
+      }
+    )
+    reqAddressuri  = URI.parse("https://map.yahooapis.jp/geoapi/V1/reverseGeoCoder?#{reqAddressprm}")
+    resAddress     = Net::HTTP.get(reqAddressuri)
+    resAddress_j   = JSON.parse(resAddress)
+    addressinfo    = resAddress_j["Feature"][0]["Property"]["Address"]
+  end
+
+  def getShops(lat, lon)
+    reqShopsprm  = URI.encode_www_form(
+      {
+        appid: $clientid,
+        query: 'ラーメン',
+        lat: lat,
+        lon: lon,
+        results: 3,
+        output: 'json',
+        sort: 'dist',
+        dist: 1
+      }
+    )
+    reqShopsuri  = URI.parse("https://map.yahooapis.jp/search/local/V1/localSearch?#{reqShopsprm}")
+    resShops    = Net::HTTP.get(reqShopsuri)
+    resShops_j   = JSON.parse(resShops)
+  end
+end
--- app/controllers/samplelinebot_controller.rb
+++ app/controllers/samplelinebot_controller.rb
@@ -24,16 +24,43 @@ class SamplelinebotController < ApplicationController
 
     events = client.parse_events_from(body)
 
-    #############
-    # オウム返し #
-    #############
+    
+    #####################
+    ### APIを使ったbot ###
+    #####################
     events.each do |event|
+      shops = getShops(event.message['latitude'], event.message['longitude'])
+      columns = []
+      i = 0
+      while i < shops['ResultInfo']['Count']
+        latlon = shops['Feature'][i]['Geometry']['Coordinates'].split(",")
+        columns.push(
+          {
+            text: shops['Feature'][i]['Name'], 
+            actions: [
+              {
+                type: "uri", 
+                label: "地図を表示", 
+                uri: "https://www.google.co.jp/maps/@#{latlon[1]},#{latlon[0]},20z"
+              }
+            ]
+          }
+        )
+        i = i + 1
+      end
+      
       message = {
-        type: 'text',
-        text: event.message['text'] 
-      }
+          type: "template",
+          altText: "this is a carousel template",
+          template: {
+            type: "carousel",
+            actions: [],
+            columns: columns
+          }
+        }
+      
       client.reply_message(event['replyToken'], message)
-    end
+      end
     
     head :ok
   end
